390cb1733682303a36a65c6c0ec905f5506b4705
aadil42 <adilkelawala@gmail.com>
2023-06-26 14:14:53 +0530

    wrapping layout around all pages. using _app.js file for that in page direcotry

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+// getting layout
+
+import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
