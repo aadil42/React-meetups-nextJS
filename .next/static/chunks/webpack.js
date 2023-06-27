dc5f3c58d3258e6846a1be541120a65984e54aa9
aadil42 <adilkelawala@gmail.com>
2023-06-27 13:17:59 +0530

    added new component EventDetail. displaying appropriate detail via that component on eventId page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f3917c9882b4cf20"; }
+/******/ 		__webpack_require__.h = function() { return "b1f527c7632a6518"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 const Test = () => {
-    return <h1>this is from meedupId directory hoo</h1>
+    return <MeetupDetail 
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        title="some title"
+        address="some address"
+        description="some description"
+    />
 }
 
 export default Test;
